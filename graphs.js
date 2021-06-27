1e02d5545b5e59310c77201cbbe07fbd36a8714a
LunarCoffee <lunarcoffee.pjc@gmail.com>
2021-06-26 23:55:11 -0400

    actrual data and stuff

--- graphs.js
+++ graphs.js
@@ -1,162 +1,164 @@
 let pieChart = document.getElementById('pieChart').getContext('2d');
 let barChart = document.getElementById('barChart').getContext('2d');
 
-let timeDistributionChart = new Chart(pieChart, {
-    type: 'pie',
-    data: {
-        labels: ['Productivity', 'Entertainment', 'Social', 'Shopping', 'Education', 'Idle'],
-        datasets: [{
-            label: 'Time Spent (minutes)',
-            data: [
-                prodMins,
-                enterMins,
-                socialMins,
-                shopMins,
-                edMins,
-                idleMins
-            ],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255,206, 86, 1)',
-                'rgba(75, 206, 86, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1,
-            borderColor: '#777',
-            hoverBorderWidth: 3,
-            hoverBorderColor: '#000',
-            hoverOffset: 5
-        }]
-    },
-    options: {
-        plugins: {
-            title: {
-                display: true,
-                align: 'center',
-                text: 'Time Spent',
-                font: {
-                    family: "Lato",
-                    size: 25,
-                },
-                color: 'black'
-            },
-            legend: {
-                display: true,
-                position: 'top',
-                labels: {
+setTimeout(() => {
+    let timeDistributionChart = new Chart(pieChart, {
+        type: 'pie',
+        data: {
+            labels: ['Productivity', 'Entertainment', 'Social', 'Shopping', 'Education', 'Idle'],
+            datasets: [{
+                label: 'Time Spent (minutes)',
+                data: [
+                    prodMins,
+                    enterMins,
+                    socialMins,
+                    shopMins,
+                    edMins,
+                    idleMins
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255,206, 86, 1)',
+                    'rgba(75, 206, 86, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1,
+                borderColor: '#777',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000',
+                hoverOffset: 5
+            }]
+        },
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    align: 'center',
+                    text: 'Time Spent',
                     font: {
                         family: "Lato",
-                        size: 18,
+                        size: 25,
                     },
                     color: 'black'
+                },
+                legend: {
+                    display: true,
+                    position: 'top',
+                    labels: {
+                        font: {
+                            family: "Lato",
+                            size: 18,
+                        },
+                        color: 'black'
+                    }
+                },
+                tooltip: {
+                    enabled: true,
+                    bodyFont: {
+                        family: "Lato"
+                    },
+                    boxWidth: 20,
+                    boxHeight: 10
                 }
             },
-            tooltip: {
-                enabled: true,
-                bodyFont: {
-                    family: "Lato"
-                },
-                boxWidth: 20,
-                boxHeight: 10
-            }
-        },
-        layout: {
-            padding: {
-                left: 0,
-                right: 0,
-                bottom: 0,
-                top: 0
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    top: 0
+                }
             }
         }
-    }
-});
+    });
 
-let timeByTabChart = new Chart(barChart, {
-    type: 'bar',
-    data: {
-        labels: ['Productivity', 'Entertainment', 'Social', 'Shopping', 'Education', 'Idle'],
-        datasets: [{
-            label: 'Time Spent (minutes)',
-            data: [
-                prodMins,
-                enterMins,
-                socialMins,
-                shopMins,
-                edMins,
-                idleMins
-            ],
-            backgroundColor: [
-                'rgba(255, 99, 132, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255,206, 86, 1)',
-                'rgba(75, 206, 86, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1,
-            borderColor: '#777',
-            hoverBorderWidth: 3,
-            hoverBorderColor: '#000',
-            hoverOffset: 70
-        }]
-    },
-    options: {
-        plugins: {
-            title: {
-                display: true,
-                align: 'center',
-                text: 'Minutes Spent per Tab Classification',
-                font: {
-                    family: "Lato",
-                    size: 25,
-                },
-                color: 'black'
-            },
-            legend: {
-                display: false,
-            },
-            tooltip: {
-                enabled: true,
-                bodyFont: {
-                    family: "Lato"
-                },
-                titleFont: {
-                    family: "Lato"
-                },
-                boxWidth: 20,
-                boxHeight: 10
-            }
+    let timeByTabChart = new Chart(barChart, {
+        type: 'bar',
+        data: {
+            labels: ['Productivity', 'Entertainment', 'Social', 'Shopping', 'Education', 'Idle'],
+            datasets: [{
+                label: 'Time Spent (minutes)',
+                data: [
+                    prodMins,
+                    enterMins,
+                    socialMins,
+                    shopMins,
+                    edMins,
+                    idleMins
+                ],
+                backgroundColor: [
+                    'rgba(255, 99, 132, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255,206, 86, 1)',
+                    'rgba(75, 206, 86, 1)',
+                    'rgba(153, 102, 255, 1)',
+                    'rgba(255, 159, 64, 1)'
+                ],
+                borderWidth: 1,
+                borderColor: '#777',
+                hoverBorderWidth: 3,
+                hoverBorderColor: '#000',
+                hoverOffset: 70
+            }]
         },
-        layout: {
-            padding: {
-                left: 0,
-                right: 0,
-                bottom: 0,
-                top: 0
-            }
-        },
-        indexAxis: 'y',
-        scales: {
-            y: {
-                ticks: {
+        options: {
+            plugins: {
+                title: {
+                    display: true,
+                    align: 'center',
+                    text: 'Minutes Spent per Tab Classification',
                     font: {
                         family: "Lato",
-                        size: 14
+                        size: 25,
                     },
-                    color: "black"
+                    color: 'black'
+                },
+                legend: {
+                    display: false,
+                },
+                tooltip: {
+                    enabled: true,
+                    bodyFont: {
+                        family: "Lato"
+                    },
+                    titleFont: {
+                        family: "Lato"
+                    },
+                    boxWidth: 20,
+                    boxHeight: 10
                 }
             },
-            x: {
-                ticks: {
-                    font: {
-                        family: "Lato",
-                        size: 14
-                    },
-                    color: "black"
+            layout: {
+                padding: {
+                    left: 0,
+                    right: 0,
+                    bottom: 0,
+                    top: 0
+                }
+            },
+            indexAxis: 'y',
+            scales: {
+                y: {
+                    ticks: {
+                        font: {
+                            family: "Lato",
+                            size: 14
+                        },
+                        color: "black"
+                    }
+                },
+                x: {
+                    ticks: {
+                        font: {
+                            family: "Lato",
+                            size: 14
+                        },
+                        color: "black"
+                    }
                 }
             }
         }
-    }
-});
+    });
+}, RACE_CONDITION_GAMING) // race condition gamign 2 elecrtic boogaloo
--- stats.js
+++ stats.js
@@ -1,64 +1,116 @@
-let prodMins = 3;
-let enterMins = 6;
-let socialMins = 3;
-let shopMins = 2;
-let edMins = 4;
-let idleMins = 1;
+let timeline;
 
-let total = prodMins + enterMins + idleMins + edMins + shopMins + socialMins;
-
-let mostTime = Math.max(prodMins, enterMins, socialMins, shopMins, edMins, idleMins);
-let mostTimeType = '';
-
-let leastTime = Math.min(prodMins, enterMins, socialMins, shopMins, edMins, idleMins);
-let leastTimeType = '';
-
-switch (mostTime) {
-    case prodMins:
-        mostTimeType = 'Productivity';
-        break;
-    case enterMins:
-        mostTimeType = 'Entertainment';
-        break;
-    case socialMins:
-        mostTimeType = 'Social';
-        break;
-    case shopMins:
-        mostTimeType = 'Shopping';
-        break;
-    case edMins:
-        mostTimeType = 'Education';
-        break;
-    case idleMins:
-        mostTimeType = 'Idle';
+function setTimeline(tl) {
+    timeline = tl;
 }
 
-switch (leastTime) {
-    case prodMins:
-        leastTimeType = 'Productivity';
-        break;
-    case enterMins:
-        leastTimeType = 'Entertainment';
-        break;
-    case socialMins:
-        leastTimeType = 'Social';
-        break;
-    case shopMins:
-        leastTimeType = 'Shopping';
-        break;
-    case edMins:
-        leastTimeType = 'Education';
-        break;
-    case idleMins:
-        leastTimeType = 'Idle';
-}
+let prodSecs = 0;
+let enterSecs = 0;
+let socialSecs = 0;
+let shopSecs = 0;
+let edSecs = 0;
+let idleSecs = 0;
 
-window.onload = function(){
-    document.getElementById("mostTime").innerHTML = mostTime.toString();
-    document.getElementById("mostTimeType").innerHTML = "in " + mostTimeType;
-    document.getElementById("leastTime").innerHTML = leastTime.toString();
-    document.getElementById("leastTimeType").innerHTML = "in " + leastTimeType;
-    document.getElementById("totalTime").innerHTML = total.toString();
-};
+let prodMins;
+let enterMins;
+let socialMins;
+let shopMins;
+let edMins;
+let idleMins;
 
+const RACE_CONDITION_GAMING = 750;
 
+setTimeout(() => {
+    for (let frame of timeline.frames) {
+        let elapsedSec = Math.floor((frame.timeEnd - frame.timeStart) / 1000);
+        switch (frame.category) {
+            case "productivity":
+                prodSecs += elapsedSec;
+                break;
+            case "entertainment":
+                enterSecs += elapsedSec;
+                break;
+            case "social":
+                socialSecs += elapsedSec;
+                break;
+            case "shopping":
+                shopSecs += elapsedSec;
+                break;
+            case "educations":
+                edSecs += elapsedSec;
+                break;
+            case "idle":
+                idleSecs += elapsedSec;
+                break;
+        }
+    }
+
+    prodMins = prodSecs / 60;
+    enterMins = enterSecs / 60;
+    socialMins = socialSecs / 60;
+    shopMins = shopSecs / 60;
+    edMins = edSecs / 60;
+    idleMins = idleSecs / 60;
+    // prodMins = 3;
+    // enterMins = 6;
+    // socialMins = 3;
+    // shopMins = 2;
+    // edMins = 4;
+    // idleMins = 1;
+
+    let total = prodMins + enterMins + idleMins + edMins + shopMins + socialMins;
+
+    let mostTime = Math.max(prodMins, enterMins, socialMins, shopMins, edMins, idleMins);
+    let mostTimeType = '';
+
+    let leastTime = Math.min(prodMins, enterMins, socialMins, shopMins, edMins, idleMins);
+    let leastTimeType = '';
+
+    switch (mostTime) {
+        case prodMins:
+            mostTimeType = 'Productivity';
+            break;
+        case enterMins:
+            mostTimeType = 'Entertainment';
+            break;
+        case socialMins:
+            mostTimeType = 'Social';
+            break;
+        case shopMins:
+            mostTimeType = 'Shopping';
+            break;
+        case edMins:
+            mostTimeType = 'Education';
+            break;
+        case idleMins:
+            mostTimeType = 'Idle';
+    }
+
+    switch (leastTime) {
+        case prodMins:
+            leastTimeType = 'Productivity';
+            break;
+        case enterMins:
+            leastTimeType = 'Entertainment';
+            break;
+        case socialMins:
+            leastTimeType = 'Social';
+            break;
+        case shopMins:
+            leastTimeType = 'Shopping';
+            break;
+        case edMins:
+            leastTimeType = 'Education';
+            break;
+        case idleMins:
+            leastTimeType = 'Idle';
+    }
+
+    window.onload = function () {
+        document.getElementById("mostTime").innerHTML = mostTime.toString();
+        document.getElementById("mostTimeType").innerHTML = "in " + mostTimeType;
+        document.getElementById("leastTime").innerHTML = leastTime.toString();
+        document.getElementById("leastTimeType").innerHTML = "in " + leastTimeType;
+        document.getElementById("totalTime").innerHTML = total.toString();
+    };
+}, RACE_CONDITION_GAMING) // race condition gaming
